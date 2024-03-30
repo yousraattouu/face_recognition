568345fcd1e7ad3f96d381c79d32c0e2a8c228a3
yousra <yousraattou44@gmail.com>
2024-03-30 23:17:41 +0100

    add

--- detect.py
+++ detect.py
@@ -1,2 +1,15 @@
 import dlib
-print(dlib.__version__)
+from skimage import io, transform
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import glob
+import openface
+import pickle
+import os
+import sys
+import argparse
+import time
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
